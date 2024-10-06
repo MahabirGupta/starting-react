779ed90e18fde6a3ce6cd9b429a6104c15eff188
Mahabir Gupta new <mahabirguptanew@192.168.1.4>
2024-10-06 11:10:30 +0800

    add the styleUrls property in header-component-ts

--- Library/Mobile Documents/com~apple~CloudDocs/Documents/angular-complete-course/angular-ekart/src/app/header/header.component.ts
+++ Library/Mobile Documents/com~apple~CloudDocs/Documents/angular-complete-course/angular-ekart/src/app/header/header.component.ts
@@ -4,6 +4,13 @@ import { Component } from '@angular/core';
 @Component({
   // every @Component must have a view template
   selector: 'app-header',
-  template: '<h3>eKart</h3>',
+  // template: '<h3>eKart</h3>',
+  templateUrl: './header.component.html',
+  // styles: [
+  //   'a{text-decoration:none; margin:0 10px;}',
+  //   'button{padding: 10px 20px;}',
+  //   '.ekart--header{width: 100%; height:70px}',
+  // ]
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
