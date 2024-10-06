ece9dd7440f3233e874ed8cfdcb173f8b4cad7f5
Mahabir Gupta new <mahabirguptanew@192.168.1.4>
2024-10-06 15:56:08 +0800

    use html attribute, css class and css id

--- Library/Mobile Documents/com~apple~CloudDocs/Documents/angular-complete-course/angular-ekart/src/app/top-header/top-header.component.ts
+++ Library/Mobile Documents/com~apple~CloudDocs/Documents/angular-complete-course/angular-ekart/src/app/top-header/top-header.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-top-header',
+  // use as a html tag
+  // selector: 'app-top-header',
+  // use like a html attribute
+  // selector: '[app-top-header]',
+  // use as a css class
+  // selector: '.app-top-header',
+  // use as a css id
+  selector: '#app-top-header',
   templateUrl: './top-header.component.html',
-  styleUrls: ['./top-header.component.css']
+  styleUrls: ['./top-header.component.css'],
 })
-export class TopHeaderComponent {
-
-}
+export class TopHeaderComponent {}
